9d74e453bdee3cf3dec0f8429f1d70f8bdd2a5be
qkuk <Luthfihadi78@gmail.com>
2023-11-30 15:26:00 +0700

    Update Login Fragment and Search Fragment

--- app/src/main/java/com/dicoding/qurbanin/ui/authentication/LoginFragment.kt
+++ app/src/main/java/com/dicoding/qurbanin/ui/authentication/LoginFragment.kt
@@ -1,60 +1,93 @@
 package com.dicoding.qurbanin.ui.authentication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.dicoding.qurbanin.R
+import com.dicoding.qurbanin.data.UserData
+import com.dicoding.qurbanin.databinding.FragmentLoginBinding
+import com.dicoding.qurbanin.ui.home.HomeFragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        _binding = FragmentLoginBinding.inflate(inflater,container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.reference.child("users")
+
+        binding.materialButton.setOnClickListener {
+            val loginEmail = binding.edtEmail.text.toString()
+            val loginPassword = binding.edtPassword.text.toString()
+
+            if (loginEmail.isNotEmpty() && loginPassword.isNotEmpty()) {
+                loginUser(loginEmail,loginPassword)
+            } else {
+                Toast.makeText(requireContext(),"Login Failed",Toast.LENGTH_SHORT).show()
+
             }
+        }
+
+        binding.tvCreateAccount.setOnClickListener {
+            it.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+    }
+
+
+    private fun loginUser(email:String, password:String) {
+        databaseReference.orderByChild(email).equalTo(email).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (user in snapshot.children) {
+                        val userData = user.getValue(UserData::class.java)
+
+                        if (userData != null && userData.password == password ){
+                            Toast.makeText(requireContext(), "Login Successful", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(requireActivity(), HomeFragment::class.java))
+                            requireActivity().finish()
+                            return
+                        }
+                    }
+                }
+                Toast.makeText(requireContext(), "Login Failed", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(),"Database Error : ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
