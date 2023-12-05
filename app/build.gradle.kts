cdb086aa6dc83ecc6fc1ee5c9c0696a97e8a5462
qkuk <Luthfihadi78@gmail.com>
2023-12-05 19:36:16 +0700

    Update Login Fragment Session

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,7 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
 }
--- app/src/main/java/com/dicoding/qurbanin/ui/authentication/LoginFragment.kt
+++ app/src/main/java/com/dicoding/qurbanin/ui/authentication/LoginFragment.kt
@@ -7,9 +7,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.dicoding.qurbanin.R
+import com.dicoding.qurbanin.core.utils.datastore.SettingPreferences
+import com.dicoding.qurbanin.core.utils.datastore.datastore
 import com.dicoding.qurbanin.data.UserData
 import com.dicoding.qurbanin.databinding.FragmentLoginBinding
 import com.dicoding.qurbanin.ui.home.HomeFragment
@@ -18,6 +26,9 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.core.Context
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class LoginFragment : Fragment() {
@@ -26,6 +37,8 @@ class LoginFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var firebaseDatabase: FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,7 +53,7 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         firebaseDatabase = FirebaseDatabase.getInstance()
-        databaseReference = firebaseDatabase.reference.child("users")
+        databaseReference = firebaseDatabase.reference.child("Users")
 
         binding.materialButton.setOnClickListener {
             val loginEmail = binding.edtEmail.text.toString()
@@ -57,9 +70,7 @@ class LoginFragment : Fragment() {
         binding.tvCreateAccount.setOnClickListener {
             it.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
-
     }
-
 
     private fun loginUser(email:String, password:String) {
         databaseReference.orderByChild(email).equalTo(email).addListenerForSingleValueEvent(object : ValueEventListener {
@@ -70,7 +81,11 @@ class LoginFragment : Fragment() {
 
                         if (userData != null && userData.password == password ){
                             Toast.makeText(requireContext(), "Login Successful", Toast.LENGTH_SHORT).show()
-                            startActivity(Intent(requireActivity(), HomeFragment::class.java))
+
+                            GlobalScope.launch {
+                                SettingPreferences.getInstance(requireContext().datastore).setLoginSession(true)
+                            }
+                            findNavController().navigate(R.id.action_loginFragment_to_homeContainerFragment)
                             requireActivity().finish()
                             return
                         }
