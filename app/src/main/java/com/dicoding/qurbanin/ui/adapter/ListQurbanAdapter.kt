7547faf3c6cc3004422c22bb33af04e652e4c9eb
Febri Aro Gea <febriarogea@gmail.com>
2023-12-14 07:45:10 +0700

    ListQurbanFragment - Logic and Display Data

---
+++ app/src/main/java/com/dicoding/qurbanin/ui/adapter/ListQurbanAdapter.kt
@@ -0,0 +1,61 @@
+package com.dicoding.qurbanin.ui.adapter
+
+import android.content.res.ColorStateList
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dicoding.qurbanin.R
+import com.dicoding.qurbanin.data.model.ListEventQurbanResponseItem
+import com.dicoding.qurbanin.databinding.ItemRegisteredQurbanBinding
+
+class ListQurbanAdapter (private val listQurban: ArrayList<ListEventQurbanResponseItem>) : RecyclerView.Adapter<ListQurbanAdapter.ViewHolder>() {
+
+    class ViewHolder(var binding: ItemRegisteredQurbanBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemRegisteredQurbanBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return listQurban.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val listQurbanItem = listQurban[position]
+
+        with(holder.binding) {
+            tvHeadlineOrganizer.text = listQurbanItem.Pelaksanaan
+            tvStatusContainer.text = listQurbanItem.StatusEvent
+
+            tvStatusContainer.backgroundTintList = when (listQurbanItem.StatusEvent) {
+                "Belum Lunas" -> ColorStateList.valueOf(holder.itemView.context.getColor(R.color.yellow))
+                "Lunas" -> ColorStateList.valueOf(holder.itemView.context.getColor(R.color.green_40))
+                "Sedang Berlangsung" -> ColorStateList.valueOf(holder.itemView.context.getColor(R.color.blue))
+                "Tersembelih" -> ColorStateList.valueOf(holder.itemView.context.getColor(R.color.purple))
+                else -> null
+            }
+
+            tvAddress.text = listQurbanItem.Location
+            tvNameStock.text = listQurbanItem.IDStock
+            tvExecutionDate.text = listQurbanItem.Pelaksanaan
+            tvDeliverTime.text = listQurbanItem.Pengambilan
+        }
+
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listQurbanItem)
+        }
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: ListEventQurbanResponseItem)
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+}
--- app/src/main/java/com/dicoding/qurbanin/ui/list_qurban/ListQurbanFragment.kt
+++ app/src/main/java/com/dicoding/qurbanin/ui/list_qurban/ListQurbanFragment.kt
@@ -5,29 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.qurbanin.R
+import com.dicoding.qurbanin.data.model.ListEventQurbanResponseItem
+import com.dicoding.qurbanin.databinding.FragmentListQurbanBinding
+import com.dicoding.qurbanin.ui.adapter.ListQurbanAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ListQurbanFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListQurbanFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentListQurbanBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var listQurbanViewModel: ListQurbanViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        // TODO: User ViewModel Factory
+        listQurbanViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[ListQurbanViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -35,26 +31,45 @@ class ListQurbanFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_qurban, container, false)
+        _binding = FragmentListQurbanBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val layoutManager = LinearLayoutManager(requireActivity())
+        binding.rvHistory.layoutManager = layoutManager
+
+        val listQurbanViewModel = ViewModelProvider(this,
+            ViewModelProvider.NewInstanceFactory()).get(ListQurbanViewModel::class.java)
+
+        listQurbanViewModel.listQurban.observe(viewLifecycleOwner) {
+            setQurbanList(it)
+        }
+    }
+
+    private fun setQurbanList(listQurban: ArrayList<ListEventQurbanResponseItem>) {
+        val adapter = ListQurbanAdapter(listQurban)
+        binding.rvHistory.adapter = adapter
+
+        adapter.setOnItemClickCallback(object : ListQurbanAdapter.OnItemClickCallback{
+
+            override fun onItemClicked(data: ListEventQurbanResponseItem) {
+                val mBundle = Bundle()
+                mBundle.putString(EXTRA_ID_EVENT,data.IDEvent)
+                view?.findNavController()?.navigate(R.id.action_listQurbanFragment_to_statusQurbanFragment,mBundle)
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListQurbanFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListQurbanFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val EXTRA_ID_EVENT = "extra_id_event"
     }
+
 }
